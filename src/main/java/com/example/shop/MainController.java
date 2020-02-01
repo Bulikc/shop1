82e86eb74c1a1bdb775ea59a6feda6b6cbb34977
goleshovivan@yandex.by <goleshovivan@yandex.by>
2020-02-01 22:23:44 +0300

    new pasha!

--- src/main/java/com/example/shop/MainController.java
+++ src/main/java/com/example/shop/MainController.java
@@ -1,5 +1,8 @@
 package com.example.shop;
 
+import com.example.shop.domain.User;
+import com.example.shop.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,18 +11,26 @@ import java.util.Map;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
             Map<String, Object> model
     ) {
-        model.put("name", name);
-        return "greeting";//!!!
+        model.put("USERS", userRepository);
+        return "index";//!!!
     }
 
     @GetMapping
     public String main(Map<String, Object> model) {
         model.put("some", "hello, letsCode!");
+        Iterable<User> users=userRepository.findAll();
+        model.put("users", users);
+
         return "index";
     }
 }
